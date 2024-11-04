d832c13e2323e41f1c4fb99d6a7dec6dfb6fbe24
Deepnshu Sharma <deepanshu0859@gmail.com>
2024-11-04 21:30:54 +0530

    first commit

---
+++ lib/demopage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DemoPageView extends StatefulWidget {
+  const DemoPageView({super.key});
+
+  @override
+  State<DemoPageView> createState() => _DemoPageViewState();
+}
+
+class _DemoPageViewState extends State<DemoPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
